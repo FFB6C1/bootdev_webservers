1484d9edb55d55147d8a43d3984568cf4df0bc03
chesca <bubblebang@gmail.com>
2024-10-21 12:08:25 +0100

    ch. 1 lesson 11

--- main.go
+++ main.go
@@ -4,10 +4,18 @@ import "net/http"
 
 func main() {
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", readinessHandler)
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serveMux,
 	}
 	server.ListenAndServe()
+}
+
+func readinessHandler(writer http.ResponseWriter, _ *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write([]byte("OK"))
 }
